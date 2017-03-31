a1896f94bb71687233c2fe11e07e074c8f83bfe0
Marcus Haagh <haagh9@hotmail.com>
2017-03-31 19:23:47 +0200

    Changed invite code to class and made IllegalInviteCode response

--- shared/src/com/fagi/model/InviteCode.java
+++ shared/src/com/fagi/model/InviteCode.java
@@ -3,7 +3,14 @@ package com.fagi.model;
 /**
  * Created by costa on 31-03-2017.
  */
-public interface InviteCode {
-    void set(int code);
-    int get();
+public class InviteCode {
+    private final int value;
+
+    public InviteCode(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
---
+++ shared/src/com/fagi/responses/IllegalInviteCode.java
@@ -0,0 +1,7 @@
+package com.fagi.responses;
+
+/**
+ * Created by costa on 31-03-2017.
+ */
+public class IllegalInviteCode implements Response {
+}
