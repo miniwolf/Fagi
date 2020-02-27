9f82152a39bd4ee8cff2672c3f3109938ff6e8d7
Marcus Haagh <haagh9@hotmail.com>
2020-02-27 23:25:54 +0100

    UserTests FriendRequests
    * Moved mocks up to setup
    * Created test that checks we call makeFriends on data when users become friends

--- fagiServer/src/test/java/com/fagi/model/UserTests.java
+++ fagiServer/src/test/java/com/fagi/model/UserTests.java
@@ -5,31 +5,41 @@ import com.fagi.responses.AllIsWell;
 import com.fagi.responses.NoSuchUser;
 import com.fagi.responses.Response;
 import com.fagi.responses.UserExists;
-import com.fagi.worker.InputAgent;
-import com.fagi.worker.OutputAgent;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class UserTests {
+    @Mock
     private Data data;
     private User user;
+    private User newFriend;
 
     @BeforeEach
     void setup() {
-        data = Mockito.spy(new Data());
         user = new User("username", "password");
+        newFriend = new User("new friend", "123");
+        doReturn(user).when(data).getUser(user.getUserName());
+        doReturn(newFriend).when(data).getUser(newFriend.getUserName());
         doReturn(new AllIsWell()).when(data).storeUser(Mockito.any());
     }
 
     @Test
     void sendingFriendRequestToAFriend_ShouldResultInUserExistsResponse() {
-        var friend = new User("friend", "123");
-        var friendReq = new FriendRequest(friend.getUserName(), new TextMessage("Hello", user.getUserName(), -1));
-        user.addFriend(friend);
+        user.addFriend(newFriend);
+        var friendReq = new FriendRequest(newFriend.getUserName(), new TextMessage("Hello", user.getUserName(), -1));
 
         Response response = user.requestFriend(data, friendReq);
 
@@ -46,11 +56,8 @@ public class UserTests {
     }
 
     @Test
-    void sendingFriendRequestToNonFriendUser_ShouldResultInUserGettingAFriendRequest() {
-        var newFriend = new User("new friend", "123");
+    void sendingFriendRequestToUserNotInFriendRequestList_ShouldResultInUserGettingAFriendRequest() {
         var friendReq = new FriendRequest(newFriend.getUserName(), new TextMessage("Hello", user.getUserName(), -1));
-
-        doReturn(newFriend).when(data).getUser(newFriend.getUserName());
 
         user.requestFriend(data, friendReq);
 
@@ -58,13 +65,9 @@ public class UserTests {
     }
 
     @Test
-    void sendingFriendRequestToUserThatHaveSendAFriendRequestToThem_ShouldResultInRemovalOfBothRequests() {
-        var newFriend = new User("new friend", "123");
+    void sendingFriendRequestToUserInFriendRequestList_ShouldResultInRemovalOfBothRequests() {
         var firstFriendRequest = new FriendRequest(user.getUserName(), new TextMessage("Hello", newFriend.getUserName(), -1));
         var secondFriendReq = new FriendRequest(newFriend.getUserName(), new TextMessage("Hello", user.getUserName(), -1));
-
-        doReturn(user).when(data).getUser(user.getUserName());
-        doReturn(newFriend).when(data).getUser(newFriend.getUserName());
 
         newFriend.requestFriend(data, firstFriendRequest);
 
@@ -74,6 +77,18 @@ public class UserTests {
                 () -> Assertions.assertFalse(user.getFriendReq().contains(firstFriendRequest)),
                 () -> Assertions.assertFalse(newFriend.getFriendReq().contains(secondFriendReq))
         );
+    }
+
+    @Test
+    void sendingFriendRequestToUserInFriendRequestList_ShouldResultInUsersBecomingFriends() {
+        var firstFriendRequest = new FriendRequest(user.getUserName(), new TextMessage("Hello", newFriend.getUserName(), -1));
+        var secondFriendReq = new FriendRequest(newFriend.getUserName(), new TextMessage("Hello", user.getUserName(), -1));
+
+        newFriend.requestFriend(data, firstFriendRequest);
+
+        user.requestFriend(data, secondFriendReq);
+
+        verify(data, times(1)).makeFriends(user, newFriend);
     }
 
     @Test
