2ff729ab563fcdb83a73f7014ce5a406493a8977
zargess <marcushaagh@gmail.com>
2024-12-01 19:45:46 +0100

    Server Unit Tests
    Removed unused MockitoExtension and Mockito strictness

--- fagiServer/src/test/java/com/fagi/model/UserTests.java
+++ fagiServer/src/test/java/com/fagi/model/UserTests.java
@@ -7,11 +7,7 @@ import com.fagi.responses.Response;
 import com.fagi.responses.UserExists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 
 import java.util.List;
 
@@ -26,8 +22,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
 public class UserTests {
     private Data data;
     private User user;
