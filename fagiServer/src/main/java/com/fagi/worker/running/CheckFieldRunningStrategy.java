f6a5f9a13279dd05eebd8cbbf0236a33b5638d6a
zargess <marcushaagh@gmail.com>
2025-04-06 18:18:30 +0200

    Worker running
    * Made OutputWorker use IsWorkerRunningStrategy to check if it's running
    * Made a method for Worker to set running field. It is package private to ensure only tests and workers set this field

--- fagiServer/src/main/java/com/fagi/worker/running/CheckFieldRunningStrategy.java
+++ fagiServer/src/main/java/com/fagi/worker/running/CheckFieldRunningStrategy.java
@@ -2,9 +2,15 @@ package com.fagi.worker.running;
 
 import com.fagi.worker.Worker;
 
+/**
+ * This strategy simply returns the value of {@link Worker#isRunning()}
+ */
 public class CheckFieldRunningStrategy implements IsWorkerRunningStrategy {
     private final Worker worker;
 
+    /**
+     * @param worker the worker the strategy should check on
+     */
     public CheckFieldRunningStrategy(Worker worker) {
         this.worker = worker;
     }
