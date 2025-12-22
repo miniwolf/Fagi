7d40720c980633df00f84e252654091ee4fab62d
zargess <marcushaagh@gmail.com>
2025-12-22 13:33:04 +0100

    Upgraded gradle and dependencies
    ## Gradle upgrade
    
    Upgraded gradle to 8.14.3. Pitest plugin blocks upgrade to gradle 9.
    
    ## Versions catalog
    
    Switched to using gradle versions catalog to easily enforce the same versions of libraries across the project.
    
    ## Upgraded dependencies and plugins
    
    Upgraded all dependencies and plugins to the latest stable versions available.
    
    ### junit-platform
    
    junit platform now uses the same version as the rest of our junit dependencies.

--- fagiServer/build.gradle.kts
+++ fagiServer/build.gradle.kts
@@ -12,8 +12,6 @@ tasks.test {
 
 dependencies {
     implementation(project(":shared"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
-    testImplementation("org.mockito:mockito-core:5.12.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.12.0")
+    testImplementation(libs.bundles.junit.base)
+    testImplementation(libs.bundles.mockito)
 }
